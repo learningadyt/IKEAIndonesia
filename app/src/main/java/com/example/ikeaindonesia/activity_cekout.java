8a2b04111370a6971ca27edd2bc884406cd57888
learningadyt <adityasuryapratamaa03@gmail.com>
2023-04-09 22:11:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ikeaindonesia/activity_cekout.java
@@ -0,0 +1,14 @@
+package com.example.ikeaindonesia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_cekout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cekout);
+    }
+}
